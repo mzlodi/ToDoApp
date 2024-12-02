1adb00ca318d670f45b45fc22205e7f12eb72001
Marija <marija.zlodi01@gmail.com>
2024-12-02 19:26:09 +0100

    Add project files.

---
+++ src/Application/TodoItems/Commands/CompleteTodoItem/CompleteTodoItem.cs
@@ -0,0 +1,31 @@
+﻿using ToDoApp.Application.Common.Interfaces;
+
+namespace ToDoApp.Application.TodoItems.Commands.CompleteTodoItem;
+
+public record CompleteTodoItemCommand : IRequest
+{
+    public int Id { get; init; }
+}
+
+public class CompleteTodoItemCommandHandler : IRequestHandler<CompleteTodoItemCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CompleteTodoItemCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task Handle(CompleteTodoItemCommand request, CancellationToken cancellationToken)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(request.Id);
+
+        if (todoItem == null)
+        {
+            throw new NotFoundException(nameof(todoItem), request.Id.ToString());
+        }
+
+        todoItem.Done = true;
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/Application/TodoItems/Commands/DeleteTodoItem/DeleteTodoItem.cs
@@ -0,0 +1,32 @@
+﻿using ToDoApp.Application.Common.Interfaces;
+
+namespace ToDoApp.Application.TodoItems.Commands.DeleteTodoItem;
+
+public record DeleteTodoItemCommand : IRequest
+{
+    public int Id { get; init; }
+}
+
+public class DeleteTodoItemCommandHandler : IRequestHandler<DeleteTodoItemCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteTodoItemCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteTodoItemCommand request, CancellationToken cancellationToken)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(request.Id);
+
+        if (todoItem == null)
+        {
+            throw new NotFoundException(nameof(todoItem), request.Id.ToString());
+        }
+
+        _context.TodoItems.Remove(todoItem);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
